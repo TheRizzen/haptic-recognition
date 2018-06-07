a5d9fe3b264f77d370c43accb2a7a8c5a4fae7ee
Thibault Miranda de Oliveira <thibault.mirandadeoliveira@gmail.com>
2018-06-07 14:54:14 +0100

    Print paper/cisors/sign

--- src/index.py
+++ src/index.py
@@ -7,11 +7,28 @@ import json
 ADDR = '192.168.33.2'
 PORT = 53451
 
+def send_vibration(finger_str):
+    payload = '{"dst": "cc:78:ab:ad:ac:0d","type": "vibration","data": {"type": "%s","dur": 655, "str": 10}}\n' % finger_str
+    payload = payload.encode('utf-8')
+    s.send(payload);
+
+def send_vibration_all():
+    send_vibration('index')
+    send_vibration('middle')
+    send_vibration('third')
+    send_vibration('little')
+
+def print_fingers(fingers):
+    print(fingers[1])
+    print(fingers[2])
+    print(fingers[3])
+    print(fingers[4])
+
 def cisors_check(fingers):
     if fingers[3]['ang'][0] >= 2 \
        and fingers[4]['ang'][0] >= 2 \
-       and fingers[1]['ang'][0] <= 1.5 \
-       and fingers[2]['ang'][0] <= 1.5 \
+       and fingers[1]['ang'][0] <= 0.3 \
+       and fingers[2]['ang'][0] <= 0.3 \
        and fingers[1]['ang'][1] <= 0.2 \
        and fingers[2]['ang'][1] >= 0:
         return True
@@ -36,6 +53,16 @@ def paper_check(fingers):
     else:
         return False
 
+def detect_sign(fingers):
+    if cisors_check(fingers):
+        return 'CISORS'
+    elif rock_check(fingers):
+        return 'ROCK'
+    elif paper_check(fingers):
+        return 'PAPER'
+    else:
+        return 'NOTHING'
+
 try:
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((ADDR, PORT))
@@ -51,21 +78,11 @@ while 1:
         pass
 
     try:
-        print(pdata['data']['fingers'][1])
-        print(pdata['data']['fingers'][2])
-        print(pdata['data']['fingers'][3])
-        print(pdata['data']['fingers'][4])
         fingers = pdata['data']['fingers'];
-        if cisors_check(fingers):
-            print('CISORS')
-        elif rock_check(fingers):
-            print('ROCK')
-        elif paper_check(fingers):
-            print('PAPER')
-        else:
-            print('NOTHING')
+        print_fingers(fingers)
+        sign = detect_sign(fingers)
+        print(sign)
     except KeyError:
         pass
 s.close()
 
-#s.send('{"dst": "cc:78:ab:ad:ac:0d","type": "vibration","data": {"type": "middle","dur": 65531, "str": 10}}\n'.encode('utf-8'));
