089d05a0e985236c4c793148bf1a5d552da7220f
Thibault Miranda de Oliveira <thibault@mirandadeoliveira.com>
2018-08-06 17:23:28 +0200

    Remove test list for signsbank init

--- signs/signs_bank.py
+++ signs/signs_bank.py
@@ -1,13 +1,19 @@
 import pickle
 
 class SignsBank:
-    def __init__(self, list_test):
-        self.signs = list_test
+    def __init__(self):
+        self.signs = []
 
     def load_from_file(self, filepath):
-        with open(filepath, 'rb') as output_file:
-            self.signs = pickle.load(output_file)
+        try:
+            with open(filepath, 'rb') as output_file:
+                self.signs = pickle.load(output_file)
+        except Exception as e:
+            print('Loading signs bank failed:', e)
 
     def dump_to_file(self, filepath):
-        with open(filepath, 'wb') as input_file:
-            pickle.dump(self.signs, input_file)
+        try:
+            with open(filepath, 'wb') as input_file:
+                pickle.dump(self.signs, input_file)
+        except Exception as e:
+            print('Dumping signs bank failed: ', e)
