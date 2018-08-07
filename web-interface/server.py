2d2f90568a1bd6a58f3b7efa0135a7ca7b935b76
Tony <anthony.abramo@epitech.eu>
2018-08-07 12:42:01 +0100

    create html file - display input
    - je créé un fichier html en python
    - j'écris mon message dans mon .html qui contient tout le html + l'input que je recois du client
    - côté client l'input est à mettre à la main
    - côté interface, l'input remplace le précédent pour l'instant, pas de concaténation des inputs
    - besoin de refresh la page web manuellement pour voir le nouvel input

--- web-interface/server.py
+++ web-interface/server.py
@@ -1,20 +1,48 @@
 #!/usr/bin/env python3
 
-# WS server example
-
+# Packages
 import asyncio
 import websockets
 
-async def hello(websocket, path):
-    name = await websocket.recv()
-    print(f"< {name}")
 
-    greeting = f"Hello {name}!"
+async def python_web_interface(websocket, path):
 
+    # on reçois l'input du client client
+    message = await websocket.recv()
+
+    # j'affiche l'input dans côté client (terminal)
+    # pour debug - peut être retiré
+    print(f"< {message}")
+    greeting = f"Hello {message}!"
+
+    # je créé un fichier html
+    f = open('helloworld.html','w')
+
+    # message qu'on passe au fichier .html
+    # contient tout le html et concatène avec
+    # l'input pour l'afficher dans le html
+    pythonHtml = """
+    <html>
+    <head></head>
+    <body>
+        <p>hello</p>"""+\
+        message +\
+    """
+    </body>
+    </html>"""
+
+    # ici je renvoie un message au client pour dire
+    # que j'ai bien reçu le message et que tout c'est bien déroulé
     await websocket.send(greeting)
     print(f"> {greeting}")
 
-start_server = websockets.serve(hello, 'localhost', 8765)
+    # j'écris mon code html + message dans mon fichier .html
+    f.write(pythonHtml)
+    f.close()
 
+# communique avec mon client
+start_server = websockets.serve(python_web_interface, 'localhost', 8765)
+
+# while 42
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
