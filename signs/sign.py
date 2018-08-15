bc971872db1b3d5ed523777b1b749d50a88e09e1
Thibault Miranda de Oliveira <thibault@mirandadeoliveira.com>
2018-08-15 22:02:11 +0200

    Add thumb checking in sign comparison

--- signs/sign.py
+++ signs/sign.py
@@ -1,14 +1,15 @@
 import math
 
-def compare_thumb(glove_finger, sign_finger):
-    pass
+PITCH_FINGER_COMPARISON_SENSIBILITY = 0.2
+YAW_FINGER_COMPARISON_SENSIBILITY = 0.1
 
 def compare_finger(glove_finger, sign_finger):
-    result = True
+    sensibilities = [PITCH_FINGER_COMPARISON_SENSIBILITY, YAW_FINGER_COMPARISON_SENSIBILITY]
     for i in range(0, 2):
-        result &= math.isclose(glove_finger[i], sign_finger[i], abs_tol=0.2)
+        if not math.isclose(glove_finger[i], sign_finger[i], abs_tol=sensibilities[i]):
+            return False
 
-    return result
+    return True
 
 class Sign:
     def __init__(self, hand=None, meaning=None):
@@ -17,12 +18,8 @@ class Sign:
 
     def compare(self, hand):
         fingers = hand.fingers.data
-        result = True
         for i in range(0, len(fingers)):
-            if i == 0:
-                pass
-                #compare_thumb(finger, self.hand.fingers.data[i])
-            else:
-                result &= compare_finger(fingers[i]['ang'], self.hand.fingers.data[i]['ang'])
+            if not compare_finger(fingers[i]['ang'], self.hand.fingers.data[i]['ang']):
+                return False
 
-        return result
+        return True
