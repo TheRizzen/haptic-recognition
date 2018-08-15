ec0e64160a9b4b132aedee49bca7e9f1b68e53b3
Thibault Miranda de Oliveira <thibault@mirandadeoliveira.com>
2018-08-15 23:35:08 +0200

    Add socket send and args for sign_matching

--- sign_matching.py
+++ sign_matching.py
@@ -1,12 +1,31 @@
+import socket
+import json
+import argparse
+
 from sensoglove import SensoGlove
 from signs.signs_bank import SignsBank
 
-glove = SensoGlove('192.168.33.2', 53451)
+parser = argparse.ArgumentParser()
+parser.add_argument('glove_host')
+parser.add_argument('glove_port', type=int)
+parser.add_argument('listener_host')
+parser.add_argument('listener_port', type=int)
+args = parser.parse_args()
+
+glove = SensoGlove(args.glove_host, args.glove_port)
 
 sb = SignsBank()
 sb.load_from_file('voc1.dat')
 
 glove.connect()
+
+socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
+socket.settimeout(5)
+try:
+    socket.connect((args.listener_host, args.listener_port))
+except OSError:
+    print('Socket connection failed with %s:%d.' % (args.listener_host, args.listener_port))
+    raise
 
 sign = None
 while 1:
@@ -15,4 +34,5 @@ while 1:
     if matching_sign is not None and matching_sign is not sign:
         sign = matching_sign
         glove.send_vibration(['thumb', 'index', 'middle', 'third', 'little'])
+        socket.send((json.dumps({'meaning': sign.meaning}) + '\n').encode())
     print(sign.meaning if sign is not None else None)
