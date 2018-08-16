df156eedbbe64a9cc391bd1f0ff615c4978a2cd5
Hugo Darses <hug.darses@epitech.eu>
2018-08-16 16:40:19 +0100

    add new way to match sign

---
+++ signs/matching_light.py
@@ -0,0 +1,31 @@
+import json
+import argparse
+
+from sensoglove import SensoGlove
+from signs.signs_bank import SignsBank
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('glove_host')
+parser.add_argument('glove_port', type=int)
+args = parser.parse_args()
+
+glove = SensoGlove(args.glove_host, args.glove_port)
+sb = SignsBank()
+sb.load_from_file('hugo.dat')
+
+glove.connect()
+
+old_meaning= 'nothing'
+def detection_sign(data):
+    matching_sign = sb.compare_with_signs(data)    
+
+    if matching_sign is not None and old_meaning is not matching_sign.meaning:
+            old_meaning == matching_sign.meaning
+            glove.send_vibration(['thumb', 'index', 'middle', 'third', 'little'])
+    print(matching_sign.meaning if matching_sign is not None else None)
+
+while 1:
+    glove.fetch_data()
+    data = glove.hand
+    detection_sign(data)
