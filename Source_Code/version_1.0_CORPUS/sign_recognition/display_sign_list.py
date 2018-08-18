1d8067479a675f2ce1818ef0c08c3119862178db
Hugo Darses <hug.darses@epitech.eu>
2018-08-18 16:55:58 +0100

    add funcion who display the list of meaning from the bank

---
+++ Source_Code/version_1.0_CORPUS/sign_recognition/display_sign_list.py
@@ -0,0 +1,18 @@
+import sys
+import os
+import argparse
+
+from signs.signs_bank import SignsBank
+from signs.sign import Sign
+
+parser = argparse.ArgumentParser()
+parser.add_argument('bank_file')
+args = parser.parse_args()
+
+bank = SignsBank()
+
+# Print all the meaning in the bank file
+bank.load_from_file(args.bank_file)
+print(bank.signs)
+for sign in bank.signs:
+    print(sign.meaning)
